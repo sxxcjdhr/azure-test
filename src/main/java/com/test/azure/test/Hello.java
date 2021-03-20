8b30ae83158acb3d012611f5f33e9e195df07b45
xiuxian.song <xiuxian.song@apexglobe.com>
2021-03-19 19:31:17 -0700

    a

---
+++ src/main/java/com/test/azure/test/Hello.java
@@ -0,0 +1,13 @@
+package com.test.azure.test;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author xiuxian.song
+ * @version 1.0
+ * @create: 2021-03-19 16:26
+ */
+@Controller
+public class Hello {
+
+}
