d62e883fe4656fb06eb3724f9ab691f1134dbaea
xiuxian.song <xiuxian.song@apexglobe.com>
2021-03-19 21:35:34 -0700

    a

--- src/main/java/com/test/azure/test/Hello.java
+++ src/main/java/com/test/azure/test/Hello.java
@@ -1,13 +1,19 @@
 package com.test.azure.test;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author xiuxian.song
  * @version 1.0
  * @create: 2021-03-19 16:26
  */
-@Controller
+@RestController
 public class Hello {
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
 
 }
